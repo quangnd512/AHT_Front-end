cfc6b52428ff0ee3e3d9187dab482e7254723332
unknown <quangnd512@gmail.com>
2021-05-06 20:30:23 +0700

    qnd - update code front-end aht

--- web/js/style.js
+++ web/js/style.js
@@ -15,3 +15,26 @@ $('.carousel__wapper').slick({
     dots: false,
     arrow: true,
 });
+
+// countdown
+$('#dountdown-active').countdown('2022/05/05', function(event) {
+    $(this).html(event.strftime('<div class="countdown-day"><span class="num">%D</span><br/> days</div><div class="countdown-hou"><span class="num">%H</span><br/> hrs</div><div class="countdown-min"><span class="num">%M</span><br/> mins</div><div class="countdown-sec"><span class="num">%S</span><br/> sec</div> '));
+});
+
+// product item
+$('.product__item').slick({
+    dots: false,
+    infinite: true,
+    arrow: true,
+    slidesPerRow: 4,
+    rows: 2,
+    prevArrow: $(".arrow-left"),
+    nextArrow: $(".arrow-right"),
+    responsive: [{
+        breakpoint: 478,
+        settings: {
+            slidesPerRow: 1,
+            rows: 1,
+        }
+    }]
+});
