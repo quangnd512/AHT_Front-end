22e26e7dff894c07adb0048d10b13e6878a9aaa8
unknown <quangnd512@gmail.com>
2021-05-11 09:03:14 +0700

    qnd - add code front-end medyplus

--- web/js/style.js
+++ web/js/style.js
@@ -1,3 +1,15 @@
+//dropdown language
+$(document).ready(function() {
+    $(".header__language").click(function() {
+        $(".header__language .header__option__list").toggleClass("active");
+    });
+    $(".header__language .header__option__list .header__option__item").click(function() {
+        var text = $(this).html();
+        $(".header__option__first").html(text);
+    });
+});
+
+//dropdown search
 $(document).ready(function() {
     $(".dropdown").click(function() {
         $(".dropdown ul").toggleClass("active");
@@ -13,11 +25,11 @@ $('.carousel__wapper').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,
-    arrow: true,
+    arrow: true
 });
 
 // countdown
-$('#dountdown-active').countdown('2022/05/05', function(event) {
+$('#dountdown--active').countdown('2022/05/05', function(event) {
     $(this).html(event.strftime('<div class="countdown-day"><span class="num">%D</span><br/> days</div><div class="countdown-hou"><span class="num">%H</span><br/> hrs</div><div class="countdown-min"><span class="num">%M</span><br/> mins</div><div class="countdown-sec"><span class="num">%S</span><br/> sec</div> '));
 });
 
@@ -31,10 +43,30 @@ $('.product__item').slick({
     prevArrow: $(".arrow-left"),
     nextArrow: $(".arrow-right"),
     responsive: [{
-        breakpoint: 478,
-        settings: {
-            slidesPerRow: 1,
-            rows: 1,
+            breakpoint: 1024,
+            settings: {
+                slidesPerRow: 3,
+                rows: 2,
+            }
+        },
+        {
+            breakpoint: 478,
+            settings: {
+                slidesPerRow: 1,
+                rows: 1,
+            }
         }
-    }]
+        // 1135       
+    ]
+});
+
+
+//Hot news
+$('.news__items').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
 });
